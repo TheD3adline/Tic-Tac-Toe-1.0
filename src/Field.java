21e51af03b5549ca331786981f713cc15ae81b4c
MichaelFessler-Laptop <michaelfessler@gmx.at>
2022-05-15 15:11:07 +0200

    Added method in Field.java to check win condition

--- src/Field.java
+++ src/Field.java
@@ -20,6 +20,67 @@ public class Field {
         System.out.println(fields[2][0] + "" + verLine + "" + fields[2][1] + "" + verLine + "" + fields[2][2]);
     }
 
+    public void move(char player, int y, int x) {
+        if(this.fields[y][x] == ' ') {
+            this.fields[y][x] = player;
+        } else {
+            System.out.println("Error: Field already occupied!");
+        }
+    }
+
+    public boolean checkWin() {
+        if((fields[0][0] == 'x') && (fields[0][1] == 'x') && (fields[0][2] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[1][0] == 'x') && (fields[1][1] == 'x') && (fields[1][2] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[2][0] == 'x') && (fields[2][1] == 'x') && (fields[2][2] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][0] == 'x') && (fields[1][0] == 'x') && (fields[2][0] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][1] == 'x') && (fields[1][1] == 'x') && (fields[2][1] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][2] == 'x') && (fields[1][2] == 'x') && (fields[2][2] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][0] == 'x') && (fields[1][1] == 'x') && (fields[2][2] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][2] == 'x') && (fields[1][1] == 'x') && (fields[2][0] == 'x')) {
+            System.out.println("Congratulations Player x, you have won the game!");
+            return true;
+        } else if((fields[0][0] == 'o') && (fields[0][1] == 'o') && (fields[0][2] == '0')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[1][0] == 'o') && (fields[1][1] == 'o') && (fields[1][2] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[2][0] == 'o') && (fields[2][1] == 'o') && (fields[2][2] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[0][0] == 'o') && (fields[1][0] == 'o') && (fields[2][0] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[0][1] == 'o') && (fields[1][1] == 'o') && (fields[2][1] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[0][2] == 'o') && (fields[1][2] == 'o') && (fields[2][2] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[0][0] == 'o') && (fields[1][1] == 'o') && (fields[2][2] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        } else if((fields[0][2] == 'o') && (fields[1][1] == 'o') && (fields[2][0] == 'o')) {
+            System.out.println("Congratulations Player o, you have won the game!");
+            return true;
+        }
+        return false;
+    }
+
     public char[][] getFields() {
         return fields;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,43 @@ public class Main {
         Field field = new Field();
 
         field.print();
+
+        System.out.println();
+
+        field.move('x', 0, 0);
+
+        field.print();
+
+        System.out.println();
+
+        field.move('x', 1, 0);
+
+        field.print();
+
+        System.out.println();
+
+        field.move('x', 2, 0);
+
+        field.print();
+
+        System.out.println();
+
+        field.move('o', 0, 2);
+
+        field.print();
+
+        System.out.println();
+
+        field.move('o', 1, 2);
+
+        field.print();
+
+        System.out.println();
+
+        field.move('o', 2, 2);
+
+        field.print();
+
+        System.out.println();
     }
 }
