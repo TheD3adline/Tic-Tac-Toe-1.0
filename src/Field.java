d88c081b81027b2126b9ebb7314d3a8d484ee2c7
MichaelFessler-Laptop <michaelfessler@gmx.at>
2022-05-17 18:03:22 +0200

    Rewrote .print() of Field.java into something less stupid

--- src/Field.java
+++ src/Field.java
@@ -1,8 +1,6 @@
 public class Field {
 
     private char[][] fields = new char[3][3];
-    private final char horLine = '-';
-    private final char verLine = '|';
 
     public Field() {
         for(int i = 0; i < 3; i++) {
@@ -13,11 +11,12 @@ public class Field {
     }
 
     public void print() {
-        System.out.println(fields[0][0] + "" + verLine + "" + fields[0][1] + "" + verLine + "" + fields[0][2]);
-        System.out.println(horLine + "" + horLine + "" + horLine + "" + horLine + "" + horLine);
-        System.out.println(fields[1][0] + "" + verLine + "" + fields[1][1] + "" + verLine + "" + fields[1][2]);
-        System.out.println(horLine + "" + horLine + "" + horLine + "" + horLine + "" + horLine);
-        System.out.println(fields[2][0] + "" + verLine + "" + fields[2][1] + "" + verLine + "" + fields[2][2]);
+        System.out.println("  A B C");
+        System.out.println("1 " + fields[0][0] + "|" + fields[0][1] + "|" + fields[0][2]);
+        System.out.println("  -----");
+        System.out.println("2 " + fields[1][0] + "|" + fields[1][1] + "|" + fields[1][2]);
+        System.out.println("  -----");
+        System.out.println("3 " + fields[2][0] + "|" + fields[2][1] + "|" + fields[2][2]);
     }
 
     public void move(char player, int y, int x) {
